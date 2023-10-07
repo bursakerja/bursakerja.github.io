b4ade0fb7cc629cd6c046ab82e89ad82f4db9bac
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-07 20:28:05 +0700

    push deh

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,4 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
 import { isiDataProposal } from "./getKonten.js";
 
-get("https://bursakerja.github.io/js/kontenProposal.json", isiDataProposal);
+get("http://127.0.0.1:5500/js/kontenProposal.json", isiDataProposal);
