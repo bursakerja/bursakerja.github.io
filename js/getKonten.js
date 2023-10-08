784a8f14512c36b545ecd82b88304e732214c9b7
Fatwaff <fax.mp4@gmail.com>
2023-10-08 08:15:11 +0700

    sip

--- js/getKonten.js
+++ js/getKonten.js
@@ -1,9 +1,9 @@
-export function isiDataProposal(results) {
+export function isiDataIndex(results) {
   const inputMapping = [
-    { id: "judul", path: "judul" },
-    { id: "anggota", path: "anggota" },
-    { id: "teamImage", path: "teamImage" },
-    // { id: "mhs1", path: "biodata_mahasiswa._id" },
+    { id: "judul", path: "index.judul" },
+    { id: "anggota1", path: "index.anggota1" },
+    { id: "anggota2", path: "index.anggota2" },
+    { id: "prodi", path: "index.prodi" },
   ];
 
   inputMapping.forEach(({ id, path, index, property }) => {
@@ -13,11 +13,42 @@ export function isiDataProposal(results) {
   });
 }
 
+export function isiDataProposal(results) {
+  const inputMapping = [
+    { id: "title", path: "proposal.title" },
+    { id: "judulproyek", path: "proposal.judulproyek" },
+    { id: "anggota1", path: "proposal.anggota1" },
+    { id: "anggota2", path: "proposal.anggota2" },
+    { id: "latarbelakang", path: "proposal.latarbelakang" },
+    { id: "deskripsiaplikasi", path: "proposal.deskripsiaplikasi" },
+    { id: "tujuan", path: "proposal.tujuan" },
+    { id: "kesimpulan", path: "proposal.kesimpulan" },
+  ];
+
+  inputMapping.forEach(({ id, path, index, property }) => {
+    const inputElement = document.getElementById(id);
+    const value = getNestedValue(results, path, index, property);
+    inputElement.innerHTML = value;
+  });
+}
+
+export function isiDataTeam(results) {
+  const inputMapping = [
+    { id: "anggota", path: "team.anggota" },
+    { id: "image", path: "team.image" },
+  ];
+
+  inputMapping.forEach(({ id, path, index, property }) => {
+    const inputElement = document.getElementById(id);
+    const value = getNestedValue(results, path, index, property);
+    inputElement.innerHTML = value;
+  });
+}
 function getNestedValue(obj, path, index, property) {
   const value = path
     .split(".")
     .reduce((value, key) => (value && value[key] ? value[key] : ""), obj);
-  console.log(`Value at path ${path}:`, value);
+  // console.log(`Value at path ${path}:`, value);
 
   if (
     Array.isArray(value) &&
